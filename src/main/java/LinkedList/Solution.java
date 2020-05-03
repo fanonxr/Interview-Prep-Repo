5ead19bb8f4e0d3bd23f1351ed475948926d3bbf
FanonX Rogers <fanonxdevelops@gmail.com>
2020-05-02 20:36:12 -0400

    Linked List solutions

---
+++ src/main/java/LinkedList/Solution.java
@@ -0,0 +1,49 @@
+package LinkedList;
+
+public class Solution {
+
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    ListNode first = dummy;
+    ListNode second = dummy;
+
+    for (int i = 1; i <= n + 1; i++) {
+      first = first.next;
+    }
+
+    while (first != null) {
+      first = first.next;
+      second = second.next;
+    }
+    second.next = second.next.next;
+    return dummy.next;
+  }
+
+  public ListNode mergeTwoList(ListNode l1, ListNode l2) {
+    ListNode dummy = new ListNode(0);
+    ListNode head = dummy;
+
+    while (l1 != null && l2 != null) {
+      if (l1.val <= l2.val) {
+        dummy.next = l1;
+        l1 = l1.next;
+      } else {
+        dummy.next = l2;
+        l2 = l2.next;
+      }
+      // update dummy
+      dummy = dummy.next;
+    }
+
+    // append the rest of the list if the any of the lists are finished
+    if (l1 != null) {
+      dummy.next = l1;
+    } else if (l2 != null) {
+      dummy.next = l2;
+    }
+
+    // return head.next because head is a ref to dummy back when it was the first ref
+    return head.next;
+  }
+}
