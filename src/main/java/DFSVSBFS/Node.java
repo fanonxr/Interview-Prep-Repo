bfdd1d427e5e6c3e927c718e44c1f0af9a91f3fe
FanonX Rogers <fanonxdevelops@gmail.com>
2020-05-04 15:50:47 -0400

    Node construction with generics

--- src/main/java/DFSVSBFS/Node.java
+++ src/main/java/DFSVSBFS/Node.java
@@ -1,5 +1,28 @@
 package DFSVSBFS;
 
-public class Node {
+import java.util.List;
 
+public class Node<T> {
+  T val;
+  List<Node<T>> adjacent;
+  public Node(T val, List<Node<T>> adjacent){
+    this.val = val;
+    this.adjacent = adjacent;
+  };
+
+  public T getVal() {
+    return val;
+  }
+
+  public List<Node<T>> getAdjacent() {
+    return adjacent;
+  }
+
+  @Override
+  public String toString() {
+    return "Node{" +
+        "val=" + val +
+        ", adjacent=" + adjacent +
+        '}';
+  }
 }
